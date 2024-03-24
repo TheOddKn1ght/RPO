aca31aef6cea4d09cd6de6cf3cbf2e98c86a6912
oddknight <oddknight404@gmail.com>
2024-03-24 22:13:04 +0300

    add apache lib

--- app/src/main/java/com/example/rpolab1/MainActivity.java
+++ app/src/main/java/com/example/rpolab1/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.rpolab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rpolab1.databinding.ActivityMainBinding;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
 
     // Used to load the 'rpolab1' library on application startup.
     static {
@@ -27,13 +46,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
 
-        int res = initRng();
-        byte[] v = randomBytes(10);
-        tv.setText(String.valueOf(v));
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -43,4 +57,15 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+    public void onButtonClick(View v)
+    {
+        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+        byte[] dec = decrypt(key, enc);
+        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
 }
